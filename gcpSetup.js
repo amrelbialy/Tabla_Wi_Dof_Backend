ebb77cd2ed574490cadbc43e14260f0d817eccdf
Amr <amrelbialy26@gmail.com>
2020-06-25 22:05:31 +0200

    test

--- gcpSetup.js
+++ gcpSetup.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 
 fs.writeFile(
   process.env.GCP_KEY_FILE,
-  process.env.GOOle_APPLICATION_CREDENTIALS,
+  process.env.GOOGle_APPLICATION_CREDENTIALS,
   err => {
     console.log(err);
   }
